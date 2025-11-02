d3dea23699d831f38c08e9971d46dce212d56f20
knownotthing <100509916+knownotthing@users.noreply.github.com>
2025-11-02 10:16:45 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/anh-huy-gas-station/', // Replace with your repo name
+  base: '/anhhuygs.github.io/', // Replace with your repo name
 })
